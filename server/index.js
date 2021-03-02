60052535e2acc4c0c2aa64374f4d0dfd0868829a
Joemn <Aruljoe37@gmail.com>
2021-03-02 06:53:34 +0530

    conected to DB

--- server/index.js
+++ server/index.js
@@ -2,9 +2,26 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import chalk from "chalk";
+import * as dotenv from "dotenv";
 
 const app = express();
+const PORT = process.env.PORT || 4000;
+dotenv.config();
+const dbUrl = process.env.DB_URL;
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+// Db Connection
+mongoose
+  .connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log(chalk.blue.bgGreen("connected"));
+    app.listen(PORT, () => {
+      console.log(chalk.blue.bgGreen(`Server is Running On ${PORT}`));
+    });
+  })
+  .catch((err) => console.log(err));
+mongoose.set("useFindAndModify", false);
